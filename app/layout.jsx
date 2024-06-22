f949456683d06a810afa54d4f58f55fa0997fa21
janpf <janpf@users.noreply.github.com>
2024-06-21 19:46:21 -0600

    github things

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
-                <link rel="icon" href="/bernd.png" />
+                <link rel="icon" href="/SuperGLEBer-site/bernd.png" />
                 <title>SuperGLEBer</title>
             </head>
 
